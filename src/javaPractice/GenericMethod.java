cbb5aba52cb0635949b364e6cb418ab7ee48ec87
pabell <pabell@PABELL03.corp.amdocs.com>
2020-08-19 02:07:06 -0500

    Update on the commit

--- src/javaPractice/GenericMethod.java
+++ src/javaPractice/GenericMethod.java
@@ -1,5 +1,45 @@
 package javaPractice;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class GenericMethod {
+	
+	class Printer
+	{
+		/*
+		 * Generic method syntax
+		 * modifier - default
+		 * property - like static (if exist)
+		 * generic -  <T> stands for Type (if exist
+		 * type - void (nothing to return)
+		 * nameOfMethod - printArray
+		 */
+		 <T> void printArray (T[] array){
+			 for (T element : array) {
+				 System.out.println(element);
+			 }
+		   
+		 }
+	 
+	}	
 
+	    public static void main( String args[] ) {
+	        Printer myPrinter = new Printer();
+	        Integer[] intArray = { 1, 2, 3 };
+	        String[] stringArray = {"Hello", "World"};
+	        myPrinter.printArray(intArray);
+	        myPrinter.printArray(stringArray);
+	        int count = 0;
+
+	        for (Method method : Printer.class.getDeclaredMethods()) {
+	            String name = method.getName();
+
+	            if(name.equals("printArray"))
+	                count++;
+	        }
+
+	        if(count > 1)System.out.println("Method overloading is not allowed!");
+	      
+	    }
+	
 }
